13c87b40fa1779e3e3d7e573daff93ab96f019fa
林伟桐 <linweitong@gemvary.com>
2018-03-19 19:27:33 +0800

    新增推送功能

--- app/src/main/java/com/izdo/MyApplication.java
+++ app/src/main/java/com/izdo/MyApplication.java
@@ -1,9 +1,14 @@
 package com.izdo;
 
+import android.util.Log;
+
 import com.izdo.util.BallListUtil;
 import com.izdo.util.Constant;
 import com.izdo.util.InitData;
 import com.mob.MobApplication;
+import com.tencent.android.tpush.XGIOperateCallback;
+import com.tencent.android.tpush.XGPushConfig;
+import com.tencent.android.tpush.XGPushManager;
 
 import cn.bmob.v3.Bmob;
 import cn.bmob.v3.update.BmobUpdateAgent;
@@ -37,6 +42,28 @@ public class MyApplication extends MobApplication {
         InitData.init(this);
         // 初始化更新公告数据
         Constant.UPDATE = InitData.readAsset(this);
+
+        // 初始化信鸽推送
+        initXGPush();
+    }
+
+    public void initXGPush() {
+        Log.d("TPush", "注册");
+        XGPushConfig.enableDebug(this, true);
+
+        XGPushManager.registerPush(this, new XGIOperateCallback() {
+            @Override
+            public void onSuccess(Object data, int flag) {
+                //token在设备卸载重装的时候有可能会变
+                Log.d("TPush", "注册成功，设备token为：" + data);
+            }
+
+            @Override
+            public void onFail(Object data, int errCode, String msg) {
+                Log.d("TPush", "注册失败，错误码：" + errCode + ",错误信息：" + msg);
+            }
+        });
+
     }
 
     public void initBmob() {
